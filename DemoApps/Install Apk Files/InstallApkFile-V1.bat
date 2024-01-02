635eca599b0a7df3336a13faa9547bdde6d95aeb
reWireByAutomation <155317313+reWireByAutomation@users.noreply.github.com>
2024-01-01 19:20:25 -0600

    Add files via upload

---
+++ DemoApps/Install Apk Files/InstallApkFile-V1.bat
@@ -0,0 +1,6 @@
+cd %ANDROID_HOME%/platform-tools 
+adb -s emulator-5554 install -r C:\AUTO-SUPPORTRESOURCES\DEMOAPPS\ApiDemos-debug.apk
+pause
+adb -s emulator-5554 install -r C:\AUTO-SUPPORTRESOURCES\DEMOAPPS\apkinfo_v2.4.8.apk
+pause
+
